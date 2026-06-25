d683e82c01cd15915f526827a1e0c8a229faffaa
Charithareddy <rcharitha132@gmail.com>
2026-06-25 11:56:28 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -4,7 +4,7 @@ class PatternDemo {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(i + " ");
+                System.out.print(i + "/t");
             }
             System.out.println();
         }
